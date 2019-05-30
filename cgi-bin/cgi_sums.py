c84066d70703da214af9185b1921f77902813dc7
dfspray <daniel_spray@comcast.net>
2019-05-30 13:10:21 -0700

    Completed cgi

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -5,5 +5,17 @@ import cgitb
 cgitb.enable()
 
 print("Content-type: text/plain")
-print()
-print("Your job is to make this work")
+print("")
+
+form = cgi.FieldStorage()
+listval = form.getlist('operand')
+try:
+    result = 0
+    for value in listval:
+        result += int(value)
+    print(str(result))
+except (ValueError, TypeError):
+    error_message = "Whoops, that wasn't supposed to happen... Please use integers"
+    print(error_message)
+
+print("Did I get 113?")
